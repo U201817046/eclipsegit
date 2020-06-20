937a2bee6f291442c3250f120bbc164961a7f3a1
U201817046 <1175531381@qq.com>
2020-06-20 16:24:13 +0800

    增加

--- test01/src/com/utils/TestMain.java
+++ test01/src/com/utils/TestMain.java
@@ -2,13 +2,13 @@ package com.utils;
 /** 
  * <p>Title: TestMain.java</p> 
  * @author smallFish 
- * @date 2020年6月18日 
+ * @date 2020骞�6鏈�18鏃� 
  * @version 1.0 
- * <p>功能描述: </p>  
- */haixng
+ * <p>鍔熻兘鎻忚堪: </p>  
+ */
 public class TestMain {
 	public static void main(String[] args) {
-			//String sqls = "insert into student values(1006,'lisi2','abc',20,'男','红伞去')";
+			//String sqls = "insert into student values(1006,'lisi2','abc',20,'鐢�','绾紴鍘�')";
 			/*String sqls = "delete from student where stu_id=1008";
 				
 			DBUtils db = new DBUtils();
