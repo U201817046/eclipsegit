5504b918e572174dfa5500d43e9e39b538d78b08
U201817046 <1175531381@qq.com>
2020-06-20 17:05:22 +0800

    修改

--- test01/src/com/utils/TestMain.java
+++ test01/src/com/utils/TestMain.java
@@ -1,5 +1,5 @@
 package com.utils;
-/** 51561
+/** 123456
  * <p>Title: TestMain.java</p> 
  * @author smallFish 
  * @date 2020骞�6鏈�18鏃� 
@@ -9,11 +9,13 @@ package com.utils;
 public class TestMain {
 	public static void main(String[] args) {
 			//String sqls = "insert into student values(1006,'lisi2','abc',20,'鐢�','绾紴鍘�')";
-			/*String sqls = "delete from student where stu_id=1008";
-				
-			DBUtils db = new DBUtils();
-			db.doUpdate(sqls);
-			db.getClose();*/
+			
+			/*
+			 * String sqls = "delete from student where stu_id=1008";
+			 * 
+			 * DBUtils db = new DBUtils(); db.doUpdate(sqls); db.getClose();
+			 */
+			 
 		String sqls = "select * from student";
 		DBUtils db = new DBUtils();
 		db.doQuery(sqls);
