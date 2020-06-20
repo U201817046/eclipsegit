9d8b86fa4333106d5901b42e613d1a70c60aa527
U201817046 <67177050+U201817046@users.noreply.github.com>
2020-06-20 16:16:31 +0800

    改一下

--- 测试01/SRC/COM/功用/TestMain.java
+++ 测试01/SRC/COM/功用/TestMain.java
@@ -1,11 +1,11 @@
 package com.utils;
-/** 
+/** 还行
  * <p>Title: TestMain.java</p> 
  * @author smallFish 
  * @date 2020年6月18日 
  * @version 1.0 
  * <p>功能描述: </p>  
- */haixng
+ */
 public class TestMain {
 	public static void main(String[] args) {
 			//String sqls = "insert into student values(1006,'lisi2','abc',20,'男','红伞去')";
